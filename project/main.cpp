0e87f446638f884e3e6052c242b0a99e8722ab11
katajima <k023g1078@g.neec.ac.jp>
2024-08-23 02:07:55 +0900

    一旦

--- project/main.cpp
+++ project/main.cpp
@@ -11,6 +11,7 @@
 #include<fstream>
 #include<sstream>
 #include<wrl.h>
+#include<externals/nlohmann/json.hpp>
 
 #include"DirectXGame/engine/struct/Structs.h"
 #include"DirectXGame/engine/math/MathFanctions.h"
@@ -141,7 +142,59 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		object3ds.push_back(object3d);
 	}
 
-	
+	// グローバル変数の保存先ファイルパス
+	const std::string kDefaultBaseDirectory = "Resources/";
+	const std::string fileName = "untitled";
+	const std::string kExtension = ".json";
+	//連結してフルパスを得る
+	const std::string fllpath = kDefaultBaseDirectory + fileName + kExtension;
+
+	// ファイルストリーム
+	std::ifstream file;
+
+	// ファイルを開く
+	file.open(fllpath);
+	// ファイルオープン失敗をチェック
+	if (file.fail()) {
+		assert(0);
+	}
+
+	// JSON文字列から解凍したデータ
+	nlohmann::json deserialized;
+
+	// 解凍
+	file >> deserialized;
+
+	// 正しいレベルデータファイルかチェック
+	assert(deserialized.is_object());
+	assert(deserialized.contains("name"));
+	assert(deserialized["name"].is_string());
+
+	// "name"を文字列として取得
+	std::string name =
+	deserialized["name"].get<std::string>();
+	// 正しいレベルデータファイルかチェック
+	assert(name.compare("scene") == 0);
+
+	// レベルデータ格納用インスタンスを生成
+	LevelData* levelData = new LevelData();
+
+	// "objects"の全オブジェクトを走査
+	for (nlohmann::json& object : deserialized["objects"]) {
+		assert(object.contains("type"));
+
+		// 種別を取得
+		std::string type = object["type"].get<std::string>();
+
+		// MESH
+		if (type.compare("MESH") == 0) {
+			// 要素追加
+			levelData->objects.
+
+		}
+
+	}
+
 	
 	//ウィンドウの×ボタンが押されるまでループ
 	while (true) {
